3e97552ccf9bb6f21f48224f4c70671e2e1e9a1b
sserver7 <3948196566@qq.com>
2026-08-10 17:21:11 +0800

    Texture Fix
    Fixes DetentionDoorBlock ,ElectricFenceBlock`s Placeholder Texture

--- src/main/java/com/sserver7/net_addiction_correct_school/item/custom/DelegationAgreementItem.java
+++ src/main/java/com/sserver7/net_addiction_correct_school/item/custom/DelegationAgreementItem.java
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
 package com.sserver7.net_addiction_correct_school.item.custom;
 
-public class delegation_agreement_item {
+public class DelegationAgreementItem {
 }
